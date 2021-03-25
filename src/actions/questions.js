7388faa6b346fe813920c72acfbaaa61ee30dc02
tatiana-passarella <tatiana.passarella@gmail.com>
2021-03-25 16:24:15 +0100

    ...writing PollQuestion component

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,5 +1,9 @@
+import { showLoading, hideLoading } from 'react-redux-loading'
+import { saveQuestionAnswer } from '../utils/api'
+
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS';
 export const ADD_ANSWER_TO_QUESTION = 'ADD_ANSWER_TO_QUESTION';
+
 
 export function receiveQuestions(questions) {
   return {
@@ -15,4 +19,17 @@ export function addAnswerToQuestion(authUser, qid, answer) {
     qid,
     answer
   };
+}
+
+export function handleSaveAnswer (qid, answer) {
+  return (dispatch, getState) => {
+      const { authUser } = getState()
+      dispatch(showLoading())
+      return saveQuestionAnswer({authUser, qid, answer})
+          .then(() => {
+              dispatch(addAnswerToQuestion(authUser, qid, answer))
+              dispatch(addAnswerToQuestion(authUser, qid, answer))
+              dispatch(hideLoading())
+          })  
+  }
 }
--- src/components/PollQuestion.js
+++ src/components/PollQuestion.js
@@ -1,27 +1,103 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { formatDate } from '../utils/helpers'
+import { handleSaveAnswer } from '../actions/questions'
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
 
 
 class PollQuestion extends Component {
+    state = {
+        selectedOption: ''
+    }
+
+    selectRadio = (e) => {
+        this.setState({
+            selectedOption: e.target.value
+        })
+    }
+
+    submitAnswer = (e) => {
+        e.preventDefault()
+        const { savePollAnswer } = this.props
+        const answer = this.state.selectedOption
+        savePollAnswer(answer)
+    }
+
+
     render () {
+        const { question, authorAvatar, timestamp, author, optionOne, optionTwo, answered, isOneAnswered, isTwoAnswered } = this.props
+        const optionOneVotes = question.optionOne.votes.length
+        const optionTwoVotes = question.optionTwo.votes.length
+        const optionOnePercentage = (optionOneVotes / (optionOneVotes + optionTwoVotes) * 100).toFixed(2)
+        const optionTwoPercentage = (optionTwoVotes / (optionOneVotes + optionTwoVotes) * 100).toFixed(2)
         console.log(this.props)
+
         return (
-           <div>
-              Hello world, I am the PollQuestion component
-           </div>
+			<Row className="justify-content-center">
+				<Col xs={12} md={6}>
+					<Card bg="light" className="m-3">
+                    { answered ? (
+						<Card.Header>
+                            <Image
+                                    src={authorAvatar}
+                                    roundedCircle
+                                    fluid
+                                    width="80"
+                                    height="80"
+                                    className="mr-2"
+                                    alt=""
+                                />
+                                {author} asks:
+						</Card.Header>
+                    ) : (
+                        <p>Not answered</p>
+                    )}
+                    </Card>
+                </Col>
+            </Row>
         )
     }
 }
 
-function mapStateToProps ({authUser, questions, users}, {id}) {
-    const question_id = questions[id]
+function mapStateToProps ({authUser, questions, users}, props) {
+    const question_id = props.match.params.id
+    const question = questions[question_id]
+    const authorAvatar = users[question.author].avatarURL
+    const author = users[question.author].id
+    const timestamp = formatDate (question.timestamp)
+    const optionOne = question.optionOne.text
+    const optionTwo = question.optionTwo.text
+    const isOneAnswered = question.optionOne.votes.includes(authUser)
+    const isTwoAnswered = question.optionTwo.votes.includes(authUser)
+    const answered = isOneAnswered || isTwoAnswered
 
     return {
+        question_id,
         users,
         questions,
         authUser,
-        question_id,
+        question,
+        authorAvatar,
+        author,
+        timestamp,
+        optionOne,
+        optionTwo,
+        isOneAnswered,
+        isTwoAnswered,
+        answered
     }
 }
 
-export default connect(mapStateToProps)(PollQuestion)
+function mapDispatchToProps (dispatch, props) {
+    const question_id = props.match.params.id
+    return {
+        savePollAnswer : (answer) => {
+            dispatch(handleSaveAnswer(question_id, answer))
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PollQuestion)
